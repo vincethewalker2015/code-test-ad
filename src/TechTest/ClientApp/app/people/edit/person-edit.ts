9291cc4a4b384921dc2803e22840f32fcf16b3a5
vincentbwalker <vincethewalker@gmail.com>
2019-04-02 15:02:15 +0000

    First Commit

---
+++ src/TechTest/ClientApp/app/people/edit/person-edit.ts
@@ -0,0 +1,53 @@
+import { autoinject } from 'aurelia-framework';
+import { Router, RouteConfig } from 'aurelia-router'
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { Person } from '../models/person';
+import { IColour } from '../interfaces/icolour';
+import { IPerson } from '../interfaces/iperson';
+
+@autoinject
+export class PersonEdit {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  private heading: string;
+  private person: Person;
+  private colourOptions: IColour[] = [];
+  private routerConfig: RouteConfig;
+
+  async activate(params, routerConfig: RouteConfig) {
+    this.routerConfig = routerConfig;
+
+    const personResponse = await this.http.fetch(`/people/${params.id}`);
+    this.personFetched(await personResponse.json());
+
+    const colourResponse = await this.http.fetch('/colours');
+    this.colourOptions = await colourResponse.json() as IColour[];
+  }
+
+  personFetched(person: IPerson): void {
+    this.person = new Person(person)
+    this.heading = `Update ${this.person.fullName}`;
+    this.routerConfig.navModel.setTitle(`Update ${this.person.fullName}`);
+  }
+
+  colourMatcher(favouriteColour: IColour, checkBoxColour: IColour) {
+    return favouriteColour.id === checkBoxColour.id;
+  }
+
+  async submit() {
+
+    // TODO: Step 7
+    //
+    // Implement the submit and save logic.
+    // Send a JSON request to the API with the newly updated
+    // this.person object. If the response is successful then
+    // the user should be navigated to the list page.
+
+    throw new Error('Not Implemented');
+  }
+
+  cancel() {
+    this.router.navigate('people');
+  }
+}
---
+++ src/TechTest/Controllers/PeopleController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TechTest.Repositories;
+using TechTest.Repositories.Models;
+
+namespace TechTest.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public PeopleController(IPersonRepository personRepository)
+        {
+            this.PersonRepository = personRepository;
+        }
+
+        private IPersonRepository PersonRepository { get; }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // TODO: Step 1
+            //
+            // Implement a JSON endpoint that returns the full list
+            // of people from the PeopleRepository. If there are zero
+            // people returned from PeopleRepository then an empty
+            // JSON array should be returned.
+
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // TODO: Step 2
+            //
+            // Implement a JSON endpoint that returns a single person
+            // from the PeopleRepository based on the id parameter.
+            // If null is returned from the PeopleRepository with
+            // the supplied id then a NotFound should be returned.
+
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, PersonUpdate personUpdate)
+        {
+            // TODO: Step 3
+            //
+            // Implement an endpoint that receives a JSON object to
+            // update a person using the PeopleRepository based on
+            // the id parameter. Once the person has been successfully
+            // updated, the person should be returned from the endpoint.
+            // If null is returned from the PeopleRepository then a
+            // NotFound should be returned.
+
+            throw new NotImplementedException();
+        }
+    }
+}
